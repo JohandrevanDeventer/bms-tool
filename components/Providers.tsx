91e36f60d8665bdc4bc533ceeee32c4db0d74a87
Johandre van Deventer <johandre.vandeventer@rubiconsa.com>
2024-12-02 09:14:49 +0200

    add Providers component for theme management

---
+++ components/Providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// import { ThemeProvider, useTheme } from "next-themes";
+import { ThemeProvider } from "./theme/ThemeProvider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      themes={["light", "dark"]}
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
