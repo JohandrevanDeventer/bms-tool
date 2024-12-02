68215aa30186a0aa04faab87d51b218d569a73ec
Johandre van Deventer <johandre.vandeventer@rubiconsa.com>
2024-12-02 09:26:22 +0200

    replace static text with HyperText component for dynamic display in Home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import HyperText from "@/components/ui/hyper-text";
 import React from "react";
 
 const Home = () => {
   const loggedIn = { firstName: "Johandré", lastName: "van Deventer" };
   return (
     <section className="landing">
-      <div className="text-5xl md:text-8xl font-bold pb-5">Hypertext</div>
+      <div className="text-5xl md:text-8xl font-bold pb-5">
+        <HyperText text="Rubicon BMS" />
+      </div>
       <div>
         <p className="font-bold italic text-lg sm:text-xl">
           Welcome to the BMS Monitoring Tool
