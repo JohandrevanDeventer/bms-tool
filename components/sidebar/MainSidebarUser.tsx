0d06b26240df022027fed091ef4833b0dcff6124
Johandre van Deventer <johandre.vandeventer@rubiconsa.com>
2024-12-02 17:25:41 +0200

    add MainSidebarUser component for user profile management in sidebar

---
+++ components/sidebar/MainSidebarUser.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import React from "react";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "../ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  CreditCard,
+  LogOut,
+  Sparkles,
+} from "lucide-react";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+
+const MainSidebarUser = ({ user }: SidebarProps) => {
+  const { isMobile } = useSidebar();
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            {user ? (
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <Avatar>
+                  <Avatar>
+                    <AvatarFallback>
+                      {user.firstName[0]} {user.lastName[0].toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                </Avatar>
+                <ChevronsUpDown className="ml-auto size-4" />
+              </SidebarMenuButton>
+            ) : (
+              <div></div>
+            )}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar>
+                  <AvatarFallback>
+                    {user.firstName[0]} {user.lastName[0].toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+
+                {/* <ClerkAvatar
+                  username={username}
+                  email={email}
+                  imageURL={imageURL}
+                /> */}
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Sparkles />
+                Upgrade to Pro
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <BadgeCheck />
+                Account
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <CreditCard />
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Bell />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+};
+
+export default MainSidebarUser;
