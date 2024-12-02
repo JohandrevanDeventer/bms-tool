1f240e9c28514361ec1feb838b06fa1713c6eacc
Johandre van Deventer <johandre.vandeventer@rubiconsa.com>
2024-12-02 13:16:52 +0200

    add DevicesRightSidebar to DashboardLayout for improved device statistics display

--- app/layouts/DashboardLayout.tsx
+++ app/layouts/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import CustomBreadcrumb from "@/components/CustomBreadcrumb";
+import DevicesRightSidebar from "@/components/DevicesRightSideDiv";
 import { ReactNode } from "react";
 
 interface DashboardLayoutProps {
@@ -15,7 +16,12 @@ const DashboardLayout = ({
       {breadcrumbSteps.length > 0 && (
         <CustomBreadcrumb steps={breadcrumbSteps} />
       )}
-      <div className="flex-1">{children}</div> {/* Main content */}
+      <div className="flex flex-col md:flex-row w-full overflow-y-auto pr-0 h-full">
+        <div className="w-full h-full">{children}</div>
+        <div className="ml-auto transition-transform duration-500 ease-in-out transform scale-95 opacity-0 md:scale-100 md:opacity-100 md:flex flex-col w-[14rem] h-full mx-2 md:sticky md:top-0 pt-4">
+          <DevicesRightSidebar />
+        </div>
+      </div>
     </div>
   );
 };
