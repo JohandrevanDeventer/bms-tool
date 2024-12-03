b11a9f9e36c0ee620cbf3aef420832f0f21f96cc
Johandre van Deventer <johandre.vandeventer@rubiconsa.com>
2024-12-03 17:17:33 +0200

    refactor: update loggedInUser variable to use getLoggedInUser function; remove hardcoded user data

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,24 +13,24 @@ export default async function RootLayout({
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
 
-  const loggedInUser = await getLoggedInUser();
-  const loggedIn = {
-    $id: "1",
-    email: "johandre.vanDeventer@rubiconsa.com",
-    userId: "1",
-    username: "JohandrevD",
-    firstName: "Johandre",
-    lastName: "van Deventer",
-    name: "Johandre van Deventer",
-    idNumber: "1234567890",
-    address1: "123 Test St",
-    city: "Test City",
-    province: "Test Province",
-    postalCode: "1234",
-    dateOfBirth: "01/01/2000",
-  };
+  const loggedIn = await getLoggedInUser();
+  // const loggedIn = {
+  //   $id: "1",
+  //   email: "johandre.vanDeventer@rubiconsa.com",
+  //   userId: "1",
+  //   username: "JohandrevD",
+  //   firstName: "Johandre",
+  //   lastName: "van Deventer",
+  //   name: "Johandre van Deventer",
+  //   idNumber: "1234567890",
+  //   address1: "123 Test St",
+  //   city: "Test City",
+  //   province: "Test Province",
+  //   postalCode: "1234",
+  //   dateOfBirth: "01/01/2000",
+  // };
 
-  if (!loggedInUser) redirect("/sign-in");
+  if (!loggedIn) redirect("/sign-in");
 
   return (
     <main className="flex h-screen w-full bg-primary font-inter">
